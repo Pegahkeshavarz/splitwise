8b13e685379bce993bd95b2ed606557718d98ff7
pegahkeshavarz <pkeshavarz65@gmail.com>
2024-04-04 16:11:06 -0700

    wip

--- src/components/register.js
+++ src/components/register.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react'
-import { Navigate, Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../authContexts'
+import {Link} from 'react-router-dom'
 import { doCreateUserWithEmailAndPassword } from '../firebase/auth'
 
 const Register = () => {
-
-    const navigate = useNavigate()
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setconfirmPassword] = useState('')
     const [isRegistering, setIsRegistering] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
-
-    const {userLoggedIn} = useAuth()
 
     const onSubmit = async (e) => {
         e.preventDefault()
